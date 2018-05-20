14c97f4f823e0be5c298421fd3ebe268d09848f5
Maigan Pierce <maiganpierce@gmail.com>
2018-05-20 11:04:13 -0500

    setup template

---
+++ app/main.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('angularApp').controller('Main', main);
+
+    function main() {
+        var vm = this;
+        vm.food = 'pizza';
+	vm.side = 'cheese';
+    }
+})();
